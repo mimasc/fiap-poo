891d08ed593eb2ad4880736a9ce9be13c69a1ce0
mimasc <mirella.mascarenhas1@gmail.com>
2026-03-02 08:39:13 -0300

    Integrate ArCondicionado functionality into main

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -21,31 +21,49 @@ package br.com.fiapride.main;
 //
 //    }
 //}
-import br.com.fiapride.model.Passageiro;
+//import br.com.fiapride.model.Passageiro;
+//
+//public class SistemaPrincipal {
+//
+//    public static void main(String[] args) {
+//        // INSTANCIAÇÃO
+//        // O comando 'new' aloca memória para um novo objeto.
+//        // Criando o primeiro passageiro (Objeto 1)
+//        Passageiro passageiro1 = new Passageiro("Ana Silva", "222");
+//        System.out.println("Regarga passageiro 1");
+//        passageiro1.adicionarSaldo(50.0);
+//
+//        // Criando o segundo passageiro (Objeto 2)
+//        Passageiro passageiro2 = new Passageiro("Carlos Souza", "333");
+//        System.out.println("Regarga passageiro 2");
+//        passageiro2.adicionarSaldo(12.5);
+//
+//        System.out.println("--- Sistema FiapRide ---");
+//        System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$ " + passageiro1.saldo + " | CPF: " + passageiro1.cpf);
+//        System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$ " + passageiro2.saldo + " | CPF: " + passageiro2.cpf);
+//        
+//        System.out.println("Pagando viagem do passageiro 1");
+//        passageiro1.pagarViagem(20);
+//        System.out.println("Pagando viagem do passageiro 2");
+//        passageiro2.pagarViagem(20);
+//
+//    }
+//}
+
+import br.com.fiapride.model.ArCondicionado;
 
 public class SistemaPrincipal {
 
     public static void main(String[] args) {
-        // INSTANCIAÇÃO
-        // O comando 'new' aloca memória para um novo objeto.
-        // Criando o primeiro passageiro (Objeto 1)
-        Passageiro passageiro1 = new Passageiro("Ana Silva", "222");
-        System.out.println("Regarga passageiro 1");
-        passageiro1.adicionarSaldo(50.0);
-
-        // Criando o segundo passageiro (Objeto 2)
-        Passageiro passageiro2 = new Passageiro("Carlos Souza", "333");
-        System.out.println("Regarga passageiro 2");
-        passageiro2.adicionarSaldo(12.5);
-
-        System.out.println("--- Sistema FiapRide ---");
-        System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$ " + passageiro1.saldo + " | CPF: " + passageiro1.cpf);
-        System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$ " + passageiro2.saldo + " | CPF: " + passageiro2.cpf);
+        ArCondicionado meuAr = new ArCondicionado();
+        meuAr.marca = "Samsung";
         
-        System.out.println("Pagando viagem do passageiro 1");
-        passageiro1.pagarViagem(20);
-        System.out.println("Pagando viagem do passageiro 2");
-        passageiro2.pagarViagem(20);
-
+        // Usando o método com True/False
+        meuAr.ligarDesligar(true);  // Isso vai ligar o ar
+        meuAr.setTemperatura(22);
+        
+        meuAr.ligarDesligar(false); // Isso vai desligar o ar
+        
+        System.out.println("Estado final: Ligado = " + meuAr.ligado);
     }
 }
