4b9254f58940c7cfd0d2be6ea4644f87527c838c
mimasc <mirella.mascarenhas1@gmail.com>
2026-03-02 08:39:52 -0300

    Refactor ArCondicionado class with methods for control

--- src/br/com/fiapride/model/ArCondicionado.java
+++ src/br/com/fiapride/model/ArCondicionado.java
@@ -1,10 +1,23 @@
 package br.com.fiapride.model;
-public class ArCondicionado { // <-- Use o nome do SEU objeto    
-    // As características que você pensou    
+
+public class ArCondicionado {
     public String marca;
     public String cor;
     public int potenciaBTU;
     public int temperaturaDesejada;
     public boolean ligado;
-    
+
+    public void ligarDesligar(boolean valor) {
+        this.ligado = valor;
+        if (this.ligado == true) {
+            System.out.println("O ar " + marca + " agora está LIGADO.");
+        } else {
+            System.out.println("O ar " + marca + " agora está DESLIGADO.");
+        }
+    }
+
+    public void setTemperatura(int novaTemperatura) {
+        this.temperaturaDesejada = novaTemperatura;
+        System.out.println("Temperatura alterada para: " + temperaturaDesejada + "°C");
+    }
 }
