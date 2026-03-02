416713699357eb993e1ec02b57deaa953b31aa4f
mimasc <mirella.mascarenhas1@gmail.com>
2026-03-02 10:23:54 -0300

    Refactor ArCondicionado class for encapsulation

--- src/br/com/fiapride/model/ArCondicionado.java
+++ src/br/com/fiapride/model/ArCondicionado.java
@@ -1,23 +1,39 @@
 package br.com.fiapride.model;
 
 public class ArCondicionado {
-    public String marca;
-    public String cor;
-    public int potenciaBTU;
-    public int temperaturaDesejada;
-    public boolean ligado;
+    private String marca;
+    private String cor;
+    private int potenciaBTU;
+    private int temperaturaDesejada;
+    private boolean ligado;
 
-    public void ligarDesligar(boolean valor) {
-        this.ligado = valor;
-        if (this.ligado == true) {
-            System.out.println("O ar " + marca + " agora está LIGADO.");
+    // Método simples para ligar/desligar com true ou false
+    public void LigarDesligar(boolean status) {
+        this.ligado = status;
+        if (this.ligado) {
+            System.out.println("Ar-condicionado LIGADO.");
         } else {
-            System.out.println("O ar " + marca + " agora está DESLIGADO.");
+            System.out.println("Ar-condicionado DESLIGADO.");
         }
     }
 
-    public void setTemperatura(int novaTemperatura) {
-        this.temperaturaDesejada = novaTemperatura;
-        System.out.println("Temperatura alterada para: " + temperaturaDesejada + "°C");
+    // Operação: adicionarTemperatura (Sobe 1 grau)
+    public void adicionarTemperatura() {
+        if (ligado) {
+            this.temperaturaDesejada++; // Aumenta em 1
+            System.out.println("Temperatura aumentada para: " + temperaturaDesejada + "°C");
+        } else {
+            System.out.println("Ligue o aparelho primeiro para mudar a temperatura!");
+        }
+    }
+
+    // Operação: diminuirTemperatura (Desce 1 grau)
+    public void diminuirTemperatura() {
+        if (ligado) {
+            this.temperaturaDesejada--; // Diminui em 1
+            System.out.println("Temperatura diminuída para: " + temperaturaDesejada + "°C");
+        } else {
+            System.out.println("Ligue o aparelho primeiro para mudar a temperatura!");
+        }
     }
 }
