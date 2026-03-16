fc2c7506dc0fbacebb57dd4072f2da257be439f0
mimasc <mirella.mascarenhas1@gmail.com>
2026-03-16 09:43:28 -0300

    Update SistemaPrincipal.java

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -71,12 +71,5 @@ public class SistemaPrincipal {
         
         // TESTE 1: A forma correta (Nascimento legalizado)        
         Veiculo meuCarro = new Veiculo("ABC-1234", "Toyota Corolla");
-        
-        // TESTE 2: Tentando burlar o sistema (Descomente a linha abaixo)
-        // Veiculo carroFantasma = new Veiculo();         
-        // 🛑 ERRO DE COMPILAÇÃO! 
-        // O Java vai sublinhar de vermelho e dizer: "The constructor Veiculo() is undefined".
-       // Por quê? Porque assim que você cria um construtor com regras, 
-       // o Java DESTRÓI aquele construtor vazio padrão. Você é obrigado a passar a placa!
    }
 }
