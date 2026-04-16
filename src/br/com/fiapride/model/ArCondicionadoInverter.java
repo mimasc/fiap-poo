e9ec01b2e8c6f589bcd2aa796e2bbd661409a3ec
mimasc <mirella.mascarenhas1@gmail.com>
2026-04-16 09:30:35 -0300

    Create ArCondicionadoInverter class for polymorphism

---
+++ src/br/com/fiapride/model/ArCondicionadoInverter.java
@@ -0,0 +1,14 @@
+// Nova classe filha para Polimorfismo
+package br.com.fiapride.model;
+
+public class ArCondicionadoInverter extends ArCondicionado {
+    public ArCondicionadoInverter(String marca, double btu) {
+        super(marca, btu);
+    }
+
+    @Override
+    public void exibirRelatorio() {
+        super.exibirRelatorio();
+        System.out.println("Tipo: Inverter - Status: Economizando 40% de energia.");
+    }
+}
